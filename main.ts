112c6acae99470616851bba4c6a7dc2293da8467
Ryan G <98589683+ryanginn@users.noreply.github.com>
2022-05-05 11:43:53 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.showString("Press A to set frequency to 22 and B to set to 33.")
+for (let index = 0; index < 50; index++) {
+    if (input.buttonIsPressed(Button.A)) {
+    	
+    } else if (input.buttonIsPressed(Button.B)) {
+    	
+    }
+}
+basic.forever(function () {
+	
+})
