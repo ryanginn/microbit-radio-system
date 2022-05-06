6ad5c464ec9399a9af0b60a6fce4e64ff92024cb
Ryan G <98589683+ryanginn@users.noreply.github.com>
2022-05-06 10:23:35 +0100

    add rx capabilities

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+radio.onReceivedString(function (receivedString) {
+    serial.writeLine("INCOMING MESSAGE!!!!")
+    basic.clearScreen()
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+    basic.pause(1000)
+    serial.writeLine(receivedString)
+    basic.showString(receivedString)
+})
 /**
  * I suggest you use PUTTY or something to read the messages via serial as reading from the Micro:bit display can be a bit of a pain.
  * 
@@ -5,6 +19,7 @@
  * 
  * -R
  */
+let msg = 0
 let frq = 0
 let optionmsg = 0
 radio.setTransmitPower(7)
@@ -36,21 +51,28 @@ for (let index = 0; index < 1212121212121212200; index++) {
             frq = 1
         }
     }
-    while (1 + 1 == 2) {
+    while (frq == 1) {
         serial.writeLine("Ready to send messages - please select a message to send by pressing A to select and B for next message")
         if (optionmsg == 0) {
-            basic.showString("H")
-            if (input.buttonIsPressed(Button.A)) {
-                radio.sendString("HELLO.")
-                serial.writeLine("\"HELLO.\" sent in seconds to channel desired.")
+            if (msg == 0) {
+                basic.showString("H")
+                basic.pause(100)
             }
-            if (input.buttonIsPressed(Button.B)) {
-                serial.writeLine("Button B pushed - loading next message")
-                basic.showString("C")
-                serial.writeLine("Message is: coming in loud and clear.")
+            if (frq == 1) {
                 if (input.buttonIsPressed(Button.A)) {
-                    radio.sendString("Coming in loud and clear")
-                    serial.writeLine("\"Coming in loud and clear\" sent in seconds to chosen channel.")
+                    radio.sendString("HELLO.")
+                    serial.writeLine("\"HELLO.\" sent in seconds to channel desired.")
+                }
+                if (input.buttonIsPressed(Button.B)) {
+                    basic.clearScreen()
+                    msg = 1
+                    serial.writeLine("Button B pushed - loading next message")
+                    basic.showString("C")
+                    serial.writeLine("Message is: coming in loud and clear.")
+                    if (input.buttonIsPressed(Button.A)) {
+                        radio.sendString("Coming in loud and clear")
+                        serial.writeLine("\"Coming in loud and clear\" sent in seconds to chosen channel.")
+                    }
                 }
             }
         }
